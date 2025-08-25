89fc5437f51470636cbd7f430367e542e7b431eb
John Kim <jkdev220@gmail.com>
2025-08-24 19:19:08 -0500

    fix: disable text selection for all buttons

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -34,7 +34,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     return (
       <Comp
         className={cn(
-          'inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+          'inline-flex select-none items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
           {
             'bg-primary text-primary-foreground hover:bg-primary/90':
               variant === 'default',
